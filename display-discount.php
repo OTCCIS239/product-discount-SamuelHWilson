420ce7c07400c2cfeef9a2ef2e776d6f4446e649
Unknown <swilsondev@outlook.com>
2018-01-30 11:23:49 -0600

    Finishing structure and seting up php form submission.

--- display-discount.php
+++ display-discount.php
@@ -1,21 +1,65 @@
+<?php
+    //Grab vars.
+    $name = filter_input(INPUT_POST, "desc");
+    $price = (double)filter_input(INPUT_POST, 'price');
+    $discountPercent = (double)filter_input(INPUT_POST, 'percent');
+
+    //Calculate discount.
+    $discountAmount = $price * ($discountPercent / 100);
+    $discountedPrice = $price - $discountAmount;
+
+    //Format.
+
+?>
+
 <!DOCTYPE html>
+
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <title>Discount Calculator</title>
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
+    <title>Discount Information</title>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row mt-5">
+            <div class="col-sm">
+            </div>
+            <div class="col-sm-8 col-md-6 col-lg-4">
+                <div class="card main-content">
+                    <h1 class="card-header h5">Product Discount Information</h1>
+                    <div class="card-body">
+                        <dl class="row">
+                            <dt class="col-sm-6">Product Description</dt>
+                            <dd class="col-sm-6"><?= htmlspecialchars($name)?></dd>
+                            <dt class="col-sm-6">List Price</dt>
+                            <dd class="col-sm-6"><?= "$".number_format($price, 2)?></dd>
+                            <dt class="col-sm-6">Discount Percent</dt>
+                            <dd class="col-sm-6"><?= $discountPercent."%"?></dd>
+                            <dt class="col-sm-6">Discount Amount</dt>
+                            <dd class="col-sm-6"><?= "$".number_format($discountAmount, 2)?></dd>
+                            <dt class="col-sm-6">Discounted Price</dt>
+                            <dd class="col-sm-6"><?= "$".number_format($discountedPrice,2)?></dd>
+                        </dl>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+            </div>
+        <div>
+    </div>
 
     <!-- jQuery first, then Tether, then Bootstrap JS. -->
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-  </body>
+</body>
+
 </html>
--- index.php
+++ index.php
@@ -9,45 +9,51 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="styles.css">
-
     <title>Discount Calculator</title>
 </head>
 
 <body>
-    <div class="container">
-        <div class="card main-content">
-            <h1 class="card-header h5">Product Discount Calculator</h1>
-            <div class="card-body">
-                <form>
-                    <div class="form-group">
-                        <div class="input-group">
-                            <input type="text" class="form-control" placeholder="Product Description">
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <div class="input-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">$</span>
-                            </div>
-                            <input type="text" class="form-control" placeholder="List Price">
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <div class="input-group">
-                            <input type="text" class="form-control" placeholder="Product Description">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">%</span>
-                            </div>
-                        </div>
-                    </div>
-
-                    <submit class="btn btn-primary">Submit</submit>
-                </form>
+    <div class="container-fluid">
+        <div class="row mt-5">
+            <div class="col-sm">
             </div>
-        </div>
+            <div class="col-sm-8 col-md-6 col-lg-4">
+                <div class="card main-content">
+                    <h1 class="card-header h5">Product Discount Calculator</h1>
+                    <div class="card-body">
+                        <form action="display-discount.php" method="post">
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <input type="text" class="form-control" name="desc" placeholder="Product Description">
+                                </div>
+                            </div>
+                
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">$</span>
+                                    </div>
+                                    <input type="text" class="form-control" name="price" placeholder="List Price">
+                                </div>
+                            </div>
+                
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <input type="text" class="form-control" name="percent" placeholder="Discount Percent">
+                                    <div class="input-group-append">
+                                        <span class="input-group-text">%</span>
+                                    </div>
+                                </div>
+                            </div>
+                
+                            <input type="submit" class="btn btn-primary" value="Calculate">
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+            </div>
+        <div>
     </div>
 
     <!-- jQuery first, then Tether, then Bootstrap JS. -->
